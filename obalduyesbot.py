94a290baddd88592088224b9c5295c685130e930
Snexyz <azizkamasterka2012@gmail.com>
2026-04-03 21:49:40 +0500

    Update obalduyesbot.py

--- obalduyesbot.py
+++ obalduyesbot.py
@@ -8,6 +8,8 @@ from telegram.ext import (
 
 TOKEN = os.getenv("TOKEN")
 
+print(TOKEN)
+
 # /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
